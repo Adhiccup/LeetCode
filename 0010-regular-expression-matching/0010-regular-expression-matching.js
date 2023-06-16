0dbd473b78c62508974fbd8721efdf74e594833b
Adhiccup <133824278+Adhiccup@users.noreply.github.com>
2023-06-16 02:36:31 -0300

    Time: 494 ms (43.39%), Space: 42.8 MB (80.00%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.js
+++ 0010-regular-expression-matching/0010-regular-expression-matching.js
@@ -4,22 +4,6 @@
  * @return {boolean}
  */
 var isMatch = function(s, p) {
-       const m = s.length;
-    const n = p.length;
-    const dp = new Array(m + 1).fill(false).map(() => new Array(n + 1).fill(false));
-    
-    dp[0][0] = true;
-
-    for (let i = 0; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (p.charAt(j - 1) === '*') {
-                dp[i][j] = dp[i][j - 2] || (i > 0 && (s.charAt(i - 1) === p.charAt(j - 2) || p.charAt(j - 2) === '.')
-                                            && dp[i - 1][j]);
-            } else {
-                dp[i][j] = i > 0 && (s.charAt(i - 1) === p.charAt(j - 1) || p.charAt(j - 1) === '.') && dp[i - 1][j - 1];
-            }
-        }
-    }
-
-    return dp[m][n];
+ p = new RegExp("^" + p + "$");
+ return p.test(s);
 };
