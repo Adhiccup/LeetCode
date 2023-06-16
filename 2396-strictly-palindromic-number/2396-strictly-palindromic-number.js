721d607c4ea20c26ef40f0ad7ce2970decc56beb
Adhiccup <133824278+Adhiccup@users.noreply.github.com>
2023-06-16 03:16:27 -0300

    Time: 58 ms (61.61%), Space: 42.2 MB (16.59%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+function isPalindrome(s) {
+  return s === s.split("").reverse().join("");
+}
+
+var isStrictlyPalindromic = function(n) {
+    for (let b = 2; b <= n - 2; b++) {
+        // Convert n to a string in base b
+        let digits = [];
+        let t = n;
+        while (t > 0) {
+          digits.push(t % b);
+          t = Math.floor(t / b);
+        }
+        let s = digits.reverse().join("");
+
+        // Check if s is a palindrome
+        if (!isPalindrome(s)) {
+          return false;
+    }
+  }
+
+  return true;
+};
