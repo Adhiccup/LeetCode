34b8bba40449e886b326087be8ee7f136257a3e0
Adhiccup <133824278+Adhiccup@users.noreply.github.com>
2023-06-16 02:58:45 -0300

    Time: 164 ms (86.11%), Space: 50.7 MB (79.80%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,6 +2,16 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    return x.toString() == x.toString().split("").reverse().join("");
+var isPalindrome = function(num) {
+  if (num < 0) { // negative numbers are not palindromes
+    return false;
+  }
+  let reversedNum = 0;
+  let originalNum = num;
+  while (originalNum > 0) {
+    const digit = originalNum % 10; // Extract the last digit of the number
+    reversedNum = (reversedNum * 10) + digit; // Append the digit to the reversed number
+    originalNum = Math.floor(originalNum / 10); // Remove the last digit from the original number
+  }
+  return num === reversedNum;
 };
