424171cc5ddb401b93d168c5e5c5ddf4a4331b31
Adhiccup <133824278+Adhiccup@users.noreply.github.com>
2023-07-03 01:50:21 -0300

    Time: 69 ms (51.74%), Space: 44.1 MB (37.84%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.js
@@ -0,0 +1,14 @@
+
+
+var buddyStrings = function(A, B) {
+    if(A.length != B.length) return false;
+    const diff = [];
+    
+    for(let i = 0; i < A.length; i++) {
+        if(A[i] != B[i]) diff.push(i);
+        if(diff.length > 2) return false;
+    }
+    if(!diff.length) return A.length != [...new Set(A)].length;
+    const [i, j] = diff; 
+    return A[i] == B[j] && B[i] == A[j];
+};
